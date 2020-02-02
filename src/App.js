75910e002210f3b9eded9c54ca707794daa761b2
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2020-02-02 03:10:11 -0700

    added all components, ready for redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React, {Fragment, useEffect} from 'react';
-import SearchBar from './components/Layout/SearchBar';
-import Logs from './components/logs/Logs';
+import React, { Fragment, useEffect } from "react";
+import SearchBar from "./components/Layout/SearchBar";
+import Logs from "./components/logs/Logs";
+import AddBtn from "./components/Layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
 
-import 'materialize-css/dist/css/materialize.min.css';
-import M from 'materialize-css/dist/js/materialize.min.js';
-import './App.css';
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
 
 const App = () => {
-  useEffect(() => {
-    // Initialize Materialize JS
-    M.AutoInit();
-  });
-  return (
-    <Fragment>
-      <SearchBar />
-      <div className="container">
-        <Logs />
-      </div>
-    </Fragment>
-  );
+ useEffect(() => {
+  // Initialize Materialize JS
+  M.AutoInit();
+ });
+ return (
+  <Fragment>
+   <SearchBar />
+   <div className="container">
+    <AddBtn />
+    <AddLogModal />
+    <EditLogModal />
+    <AddTechModal />
+    <TechListModal />
+    <Logs />
+   </div>
+  </Fragment>
+ );
 };
 
 export default App;
