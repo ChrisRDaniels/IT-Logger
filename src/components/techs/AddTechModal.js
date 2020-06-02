5402ec27c5da6c8384390c2d43ae32cbf740e0a4
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2020-06-01 18:53:54 -0600

    commit after testing

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import M from "materialize-css/dist/js/materialize.min.js";
+import { connect } from "react-redux";
+import { addTech } from "../../actions/techActions";
 
-const AddTechModal = () => {
+const AddTechModal = ({ addTech }) => {
  const [firstName, setFirstName] = useState("");
  const [lastName, setLastName] = useState("");
 
@@ -9,7 +11,12 @@ const AddTechModal = () => {
   if (firstName === " " || lastName === "") {
    M.toast({ html: "Please enter the first and last name" });
   } else {
-   console.log(firstName, lastName);
+   addTech({
+    firstName,
+    lastName
+   });
+
+   M.toast({ html: `${firstName} ${lastName} was added as a tech` });
    // Clear Fields
    setFirstName("");
    setLastName("");
@@ -61,4 +68,4 @@ const AddTechModal = () => {
  );
 };
 
-export default AddTechModal;
+export default connect(null, { addTech })(AddTechModal);
